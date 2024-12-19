69436df8de9621e985e2043d42c0c4fd12f2758f
kekW <panicua12@gmail.com>
2024-12-19 18:10:54 +0200

    add: new app(Mission), redis cache, postgress, routing

--- TT_DevelopsToday_SpyCat/urls.py
+++ TT_DevelopsToday_SpyCat/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/spycats/", include("SpyCat.urls", namespace="SpyCat")),
+    path("api/missions/", include("Mission.urls", namespace="Mission")),
 ]
